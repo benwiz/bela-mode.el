b523ef238c034f4a25756b7ab097349e28b305e6
benwiz <bwisialowski@gmail.com>
2020-02-19 17:15:08 -0600

    set up a easy command generation structure

--- bela-mode.el
+++ bela-mode.el
@@ -22,27 +22,51 @@
 
 ;;; Code:
 
-;; helpful https://nullprogram.com/blog/2013/02/06/
-
-;; TODO figure out how to turn on bela mode using .dir-locals
-;; TODO create a concat function that checks for trailing slash
+;; TODO defcustom for bbb-address. Make sure to have a good default. Maybe have a default for bela-scripts-dir, it's mainly for me anyway.
+;; TODO a flag for force overwrite. It's `--force` on build_project.sh
+;; TODO need to get project directory somehow
+;; BBB_ADDRESS=root@192.168.6.2 ./build_project.sh ../examples/Fundamentals/print/
 
 (defcustom bela-scripts-dir nil
   "The local directory containing Bela scripts (https://github.com/BelaPlatform/Bela/tree/master/scripts)."
   :type 'string
   :group 'bela-mode)
 
+(defun trailing-slash? (path)
+  "Check for trailing slash on PATH."
+  (string= "/" (substring path (- (length path) 1) (length path))))
+
+(defun fullpath (path filename)
+  "Concat PATH and FILENAME intelligently based on trailing slash."
+  (if (trailing-slash? path)
+      (concat bela-scripts-dir filename)
+    (concat bela-scripts-dir filename)))
+
+(defun command (filename)
+  "Create command using PATH and FILENAME."
+  (concat "BBB_ADDRESS=root@192.168.6.2 " (fullpath bela-scripts-dir filename)))
+
 (defun bela-my-test ()
   "Call my_test.sh."
   (interactive)
-  (shell-command (concat bela-scripts-dir "my_test.sh")))
+  (shell-command (command "my_test.sh")))
+
+(defun bela-build-project ()
+  "Call my_test.sh."
+  (interactive)
+  (shell-command (command "build_project.sh ~/code/bela-dsp/looper/")))
+
+(defun bela-stop-running ()
+  "Call my_test.sh."
+  (interactive)
+  (shell-command (command "stop_running.sh")))
 
 ;;;###autoload
 (define-minor-mode bela-mode
   "Minor mode for calling Bela scripts"
   :lighter " bela"
   :keymap (let ((map (make-sparse-keymap)))
-            (define-key map (kbd "C-c e") 'bela-my-test)
+            (define-key map (kbd "C-c t") 'bela-my-test)
             map))
 
 (provide 'bela-mode)
